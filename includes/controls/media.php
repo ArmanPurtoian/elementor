fcb349d5b32df9a5585b23a1a793ea001ab48ce1
Arman Purtoian <armanp@elementor.com>
2024-01-23 12:11:24 +0100

    Internal: General improvements [ED-13418]

--- includes/controls/media.php
+++ includes/controls/media.php
@@ -382,7 +382,7 @@ class Control_Media extends Control_Base_Multiple {
 	public static function get_image_alt( $instance ) {
 		if ( empty( $instance['id'] ) ) {
 			// For `Insert From URL` images.
-			return isset( $instance['alt'] ) ? trim( strip_tags( $instance['alt'] ) ) : '';
+			return isset( $instance['alt'] ) ? trim( esc_attr( strip_tags( $instance['alt'] ) ) ) : '';
 		}
 
 		$attachment_id = $instance['id'];
@@ -402,7 +402,7 @@ class Control_Media extends Control_Base_Multiple {
 				$alt = $attachment->post_title;
 			}
 		}
-		return trim( strip_tags( $alt ) );
+		return trim( esc_attr( strip_tags( $alt ) ) );
 	}
 
 	public function get_style_value( $css_property, $control_value, array $control_data ) {
