cf64d4715d91b5ee7d21a5409be8015b1ddc52b2
Arman Purtoian <armanp@elementor.com>
2024-01-25 11:23:15 +0100

    Internal: General improvements [ED-13402]

--- includes/template-library/manager.php
+++ includes/template-library/manager.php
@@ -425,15 +425,17 @@ class Manager {
 
 		$post_id = intval( $args['template_id'] );
 		$post = get_post( $post_id );
+		$post_status = get_post_status( $post_id );
+
 		if ( ! $post || get_post_type( $post ) !== Source_Local::CPT ) {
 			return new \WP_Error( 'template_error', esc_html__( 'Invalid template type or template does not exist', 'elementor' ) );
 		}
 
-		if ( 'private' === $post->post_status && ! current_user_can( 'read_private_posts', $post_id ) ) {
+		if ( 'private' === $post_status && ! current_user_can( 'read_private_posts', $post_id ) ) {
 			return new \WP_Error( 'template_error', esc_html__( 'You do not have permission to access this template', 'elementor' )  );
 		}
 
-		if ( 'publish' !== $post->post_status && ! current_user_can( 'edit_post', $post_id ) ) {
+		if ( 'publish' !== $post_status && ! current_user_can( 'edit_post', $post_id ) ) {
 			return new \WP_Error( 'template_error', esc_html__( 'You do not have permission to export this template', 'elementor' ) );
 		}
 
